f71950791e4f0b0cd2bf8fcd2030ab68cac50447
gongshichun <gongshichun@outlook.com>
2018-10-16 22:55:12 +0800

    添加路由noMatch配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
 import * as React from 'react'
+import { Route, Redirect, Switch } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import NoMatch from './pages/NoMatch'
 import './App.css'
 
 export interface InterfaceForApp {
   children ? : any
 }
 
+const redirectToLogin = (props: any): any => {
+  return (
+    <Redirect
+      to={{
+        pathname: '/login',
+        state: {
+          from: props.location || '/'
+        },
+      }}
+    />
+  )
+}
+
 class App extends React.Component<InterfaceForApp, {}> {
   public render() {
     return (
       <div className="app">
-        {this.props.children}
+        <Switch>
+          <Route exact={true} path="/" render={redirectToLogin}/>
+          <Route path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
       </div>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,21 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { createStore } from 'redux'
 import { Provider  } from 'react-redux'
 import rootReducer from './models/reducer'
 import App from './App'
-import Home from './pages/home'
-import Login from './pages/login'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 // creae store
 const store = createStore(rootReducer)
 // const history = syncHistoryWithStore(browserHistory, store)
-const redirectToLogin = (props: any): any => {
-  return (
-    <Redirect
-      to={{
-        pathname: '/login',
-        state: {
-          from: props.location || '/'
-        },
-      }}
-    />
-  )
-}
 ReactDOM.render(
   <Router>
-    <Switch>
-      <Provider store={store}>
-        <App>
-          <Route exact={true} path="/" render={redirectToLogin}/>
-          <Route path="/login" component={Login} />
-          <Route path="/home" component={Home} />
-        </App>
-      </Provider>
-    </Switch>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root') as HTMLElement
 );
